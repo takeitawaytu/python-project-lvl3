504be290b4ce351a7e31fbe3df47de3ceda7c442
takeitawaytu <takeitawaytu@gmail.com>
2021-11-04 17:23:06 +0300

    make some fixes

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -4,6 +4,13 @@ import os
 import sys
 import logging
 from page_loader.loader import download
+from page_loader.get_and_save_content import PageLoadError, \
+    WebError, SysError
+
+SUCCESSFUL_EXIT_CODE = 0
+COMMON_ERROR_EXIT_CODE = 1
+WEB_ERROR_EXIT_CODE = 0
+WRITE_ERROR_EXIT_CODE = 3
 
 
 def main():
@@ -20,9 +27,15 @@ def main():
     try:
         result = download(args.url, args.path)
         print(result)
-    except Exception:
-        logging.exception('Exception occurred')
-        sys.exit(1)
+    except PageLoadError as known_error:
+        logging.error(str(known_error))
+        if isinstance(known_error, WebError):
+            sys.exit(WEB_ERROR_EXIT_CODE)
+        elif isinstance(known_error, SysError):
+            sys.exit(WRITE_ERROR_EXIT_CODE)
+        else:
+            sys.exit(COMMON_ERROR_EXIT_CODE)
+    sys.exit(SUCCESSFUL_EXIT_CODE)
 
 
 if __name__ == '__main__':
